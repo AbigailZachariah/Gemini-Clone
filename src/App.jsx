1672873a1010f6f5c0f4bb9ae31977386266590e
Abigail Zachariah <abigail2005zachariah@gmail.com>
2026-08-16 16:01:29 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Sidebar from './components/Sidebar/Sidebar'
+
+const App = () => {
+  return (
+    <>
+      {/* mounted sidebar component in app component */}
+      <Sidebar/>
+    </>
+  )
+}
+
+export default App
