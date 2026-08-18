f7674616190b825897de353913299dcbdd732e7b
Abigail Zachariah <abigail2005zachariah@gmail.com>
2026-08-18 14:26:48 +0530

    Added the UI of the main component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Sidebar from './components/Sidebar/Sidebar'
+import Main from './components/Main/Main'
 
 const App = () => {
   return (
     <>
       {/* mounted sidebar component in app component */}
       <Sidebar/>
+      <Main/>
     </>
   )
 }
